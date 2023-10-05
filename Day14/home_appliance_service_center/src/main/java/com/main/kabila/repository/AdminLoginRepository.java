fa334a8e4dfdb5391f94ce576b9c4f9262c3a0ba
Kabila-Srinivasan <110532283+Kabila-Srinivasan@users.noreply.github.com>
2023-10-05 10:42:02 +0530

    Add files via upload

---
+++ Day14/home_appliance_service_center/src/main/java/com/main/kabila/repository/AdminLoginRepository.java
@@ -0,0 +1,8 @@
+package com.main.kabila.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.main.kabila.entity.AdminLogin;
+public interface AdminLoginRepository extends JpaRepository<AdminLogin,Integer> {
+
+
+
+}
---
+++ Day14/home_appliance_service_center/src/test/java/com/main/kabila/HomeApplianceServiceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.kabila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApplianceServiceCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
