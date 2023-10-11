3d4d6371b7a1851e0a8b1db001a2ddf57ebd2840
Kabila-Srinivasan <110532283+Kabila-Srinivasan@users.noreply.github.com>
2023-10-11 06:36:06 +0530

    Add files via upload

---
+++ Day16/contact_service/src/main/java/com/example/demo/service/ContactService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.Contact;
+
+public interface ContactService {
+
+	public List<Contact> getContactsOfUser(Long userId);
+
+
+}
